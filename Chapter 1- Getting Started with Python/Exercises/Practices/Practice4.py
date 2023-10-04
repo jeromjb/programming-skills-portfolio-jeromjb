9dd9355ef6b6ff2c6d7efb90aa9a8355b5a099f1
jeromjb <jeromjjb2129@gmail.com>
2023-10-04 22:47:57 +0400

    Create Practice4.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Practices/Practice4.py
@@ -0,0 +1,9 @@
+#Compute the area of the triangle
+
+import math
+a = float(input("Enter the length of side 'a':"))
+b = float(input("Enter the length of side 'b':"))
+c = float(input("Enter the length of side 'c':"))
+s = (a+b+c) / 2
+area = math.sqrt(s * (s-a) * (s-b) * (s-c))
+print(f"The area of the trianngle is {area:.2f} square units")
