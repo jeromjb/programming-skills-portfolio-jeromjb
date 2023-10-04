d417eda4b46f290f937fdeb55a22f3851b7d74c1
jeromjb <jeromjjb2129@gmail.com>
2023-10-04 22:55:47 +0400

    Create Practice5.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Practices/Practice5.py
@@ -0,0 +1,8 @@
+#Write four  strings in different variables and print the output as one string (string concatenation)
+
+s1 = "Hi, "
+s2 = "I "
+s3 = "am "
+s4 = "Jerom."
+con_string = s1 + s2 + s3 + s4
+print (con_string)
