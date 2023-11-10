8880bfa7a9bc34b3da7eb139603dec7bff891548
jeromjb <jeromjjb2129@gmail.com>
2023-11-10 20:55:38 +0400

    Create Exercise3.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise3.py
@@ -0,0 +1,4 @@
+'''Use a variable to represent a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, “\t” and “\n”, at least once.'''
+
+name = "\t  Jerom Joe Binu\n"
+print (name)
