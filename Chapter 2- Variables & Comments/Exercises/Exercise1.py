0a0a8314d2a741c35badc391fcd99a8a877dfcbb
jeromjb <jeromjjb2129@gmail.com>
2023-11-10 20:33:56 +0400

    Create Exercise1.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise1.py
@@ -0,0 +1,8 @@
+'''Assign a message to a variable, and print that message.
+Then change the value of the variable to a new message, and print the new
+message.'''
+
+x = "Today is Thursday"
+print (x)
+x = "It is a good day"
+print (x)
