1e674da92f7653d4580cbaa17ec52f590ecf4642
jeromjb <jeromjjb2129@gmail.com>
2023-11-13 11:05:36 +0400

    Create Exercise2.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise2.py
@@ -0,0 +1,11 @@
+'''Start with the list you used in Exercise 1, but instead of just
+
+printing each person’s name, print a message to them. The text of each message should be the same, but each message should be 
+
+personalized with the person’s name.'''
+
+
+friends = ["Johan","Sathwik","Alan","Sheehan","Sabah","Afnan","Alon","Akinesh","Namith"]
+for name in friends :
+    print (f"Hello, {name}!, Have a good day")
+    
