220dc587b1f716e01155707fc9e59597e5b17d8b
jeromjb <jeromjjb2129@gmail.com>
2023-11-13 10:55:06 +0400

    Delete Exercise 4.py

--- Chapter 2- Variables & Comments/Exercises/Exercise 4.py
+++
@@ -1,5 +0,0 @@
-'''Use a variable to represent a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, “\t” and “\n”, at least once.'''
-
-fav_no = 77
-x = fav_no
-print (x)
