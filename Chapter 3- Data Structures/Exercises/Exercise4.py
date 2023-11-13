ea1f12bd29b0f1e0b7cc57fc82616031d7722347
jeromjb <jeromjjb2129@gmail.com>
2023-11-13 11:15:27 +0400

    Create Exercise4.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise4.py
@@ -0,0 +1,8 @@
+'''If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d
+
+like to invite to dinner. Then use your list to print a message to each person, invitingthem to dinner.'''
+
+
+friends = ["Johan","Sathwik","Alan"]
+for name in friends :
+    print (f"Hello, {name}!, Inviting you for a dinner. Would you like to join?")
