1f240ac7e9ae6c964b397713b11d385497189d65
jeromjb <jeromjjb2129@gmail.com>
2023-11-16 01:40:25 +0400

    Update Exercise2.py

--- Chapter 4- Control Flow/Exercises/Exercise2.py
+++ Chapter 4- Control Flow/Exercises/Exercise2.py
@@ -1,5 +1,5 @@
-'''Choose a color for an alien as you did in Exercise 5-3, and write an if-else chain.
-
+'''Choose a color for an alien as you did in Exercise 5-3, 
+and write an if-else chain.
 •If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
 
 •If the alien’s color isn’t green, print a statement that the player just earned 10 points.
