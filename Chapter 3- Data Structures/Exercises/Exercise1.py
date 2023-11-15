b6f318d98cf7a35f2b98591fefec78e64e3b49b3
jeromjb <jeromjjb2129@gmail.com>
2023-11-16 01:39:58 +0400

    Update Exercise1.py

--- Chapter 3- Data Structures/Exercises/Exercise1.py
+++ Chapter 3- Data Structures/Exercises/Exercise1.py
@@ -1,6 +1,8 @@
-'''Store the names of a few of your friends in a list called names. Print each person’s name by accessing each element in the list, one at a
- time.'''
+'''Store the names of a few of your friends in a list called 
+names. Print each person’s name by accessing each element in the 
+list, one at a time.'''
 
-friends = ["Johan","Sathwik","Alan","Sheehan","Sabah","Afnan","Alon","Akinesh","Namith"]
+friends = ["Johan","Sathwik","Alan","Sheehan",
+           "Sabah","Afnan","Alon","Akinesh","Namith"]
 for name in friends :
     print (name)
