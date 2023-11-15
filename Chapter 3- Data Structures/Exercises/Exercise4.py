23c1023f705069bb32a4ac5d7629d554142af8fc
jeromjb <jeromjjb2129@gmail.com>
2023-11-16 01:40:16 +0400

    Update Exercise4.py

--- Chapter 3- Data Structures/Exercises/Exercise4.py
+++ Chapter 3- Data Structures/Exercises/Exercise4.py
@@ -1,6 +1,7 @@
-'''If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d
-
-like to invite to dinner. Then use your list to print a message to each person, invitingthem to dinner.'''
+'''If you could invite anyone, living or deceased, to dinner,
+ who would you invite? Make a list that includes at least three 
+ people you’d like to invite to dinner. Then use your list to 
+ print a message to each person, invitingthem to dinner.'''
 
 
 friends = ["Johan","Sathwik","Alan"]
