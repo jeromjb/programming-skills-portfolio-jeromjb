2b17bc2bac5b442fe52771a0c64cfed8cc4c4e99
jeromjb <jeromjjb2129@gmail.com>
2023-11-30 10:20:16 +0400

    Create Joe n' Joe's Vending Machine 2.py
    Vending Machine

---
+++ Vending Machine/Joe n' Joe's Vending Machine 2.py
@@ -0,0 +1,46 @@
+#3
+items = {
+    'item1': {
+        'category': 'Milk & Bevs',
+        'name': 'Brownie Drowns',
+        'code': 101,
+        'price': 15.00,
+        'stock': 12
+    },
+    'item2': {
+        'category': 'Packed',
+        'name': 'Larabar',
+        'code': 102,
+        'price': 10,
+        'stock': 20
+    },
+    'item3': {
+        'category': 'Juices',
+        'name': 'Mango Punch',
+        'code': 103,
+        'price': 13,
+        'stock': 15
+    },
+    
+    
+}
+
+
+print("Category:", items['item1']['category'])
+print("Name:", items['item1']['name'])
+print("Code:", items['item1']['code'])
+print("Price:", items['item1']['price'])
+print("Stock:", items['item1']['stock'])
+
+print("Category:", items['item2']['category'])
+print("Name:", items['item2']['name'])
+print("Code:", items['item2']['code'])
+print("Price:", items['item2']['price'])
+print("Stock:", items['item2']['stock']) 
+
+print("Category:", items['item3']['category'])
+print("Name:", items['item3']['name'])
+print("Code:", items['item3']['code'])
+print("Price:", items['item3']['price'])
+print("Stock:", items['item3']['stock']) 
+
